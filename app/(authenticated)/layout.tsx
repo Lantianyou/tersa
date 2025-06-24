2aded30b96bd3a2a6b107f23b8930b637f06cdc7
Lantianyou <dtlantianyou@gmail.com>
2025-06-24 12:01:02 +0800

    feat: rm providers

--- app/(authenticated)/layout.tsx
+++ app/(authenticated)/layout.tsx
@@ -1,10 +1,6 @@
 import { currentUser, currentUserProfile } from '@/lib/auth';
-import { env } from '@/lib/env';
+
 import { PostHogIdentifyProvider } from '@/providers/posthog-provider';
-import {
-  type SubscriptionContextType,
-  SubscriptionProvider,
-} from '@/providers/subscription';
 import { ReactFlowProvider } from '@xyflow/react';
 import { redirect } from 'next/navigation';
 import type { ReactNode } from 'react';
@@ -26,23 +22,12 @@ const AuthenticatedLayout = async ({ children }: AuthenticatedLayoutProps) => {
     return null;
   }
 
-  let plan: SubscriptionContextType['plan'];
-
-  if (profile.productId === env.STRIPE_HOBBY_PRODUCT_ID) {
-    plan = 'hobby';
-  } else if (profile.productId === env.STRIPE_PRO_PRODUCT_ID) {
-    plan = 'pro';
-  }
 
   return (
-    <SubscriptionProvider
-      isSubscribed={Boolean(profile.subscriptionId)}
-      plan={plan}
-    >
+
       <PostHogIdentifyProvider>
         <ReactFlowProvider>{children}</ReactFlowProvider>
       </PostHogIdentifyProvider>
-    </SubscriptionProvider>
   );
 };
 
--- providers/subscription.tsx
+++ providers/subscription.tsx
@@ -1,39 +1,15 @@
 'use client';
 
-import { type ReactNode, createContext, useContext } from 'react';
-
 export type SubscriptionContextType = {
   isSubscribed: boolean;
   plan: 'hobby' | 'pro' | 'enterprise' | undefined;
 };
 
-export const SubscriptionContext = createContext<SubscriptionContextType>({
-  isSubscribed: false,
-  plan: undefined,
-});
+
 
 export const useSubscription = () => {
-  const context = useContext(SubscriptionContext);
-
-  if (!context) {
-    throw new Error(
-      'useSubscription must be used within a SubscriptionProvider'
-    );
-  }
-
-  return context;
+  return {
+    isSubscribed: true,
+    plan: 'pro',
+  };
 };
-
-export const SubscriptionProvider = ({
-  children,
-  isSubscribed,
-  plan,
-}: {
-  children: ReactNode;
-  isSubscribed: boolean;
-  plan: 'hobby' | 'pro' | 'enterprise' | undefined;
-}) => (
-  <SubscriptionContext.Provider value={{ isSubscribed, plan }}>
-    {children}
-  </SubscriptionContext.Provider>
-);
